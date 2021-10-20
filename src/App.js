2661ad0a7757313bffcbc30a117c47dda029d7c3
Misael Abanto <jabanto@pichincha.com>
2021-10-20 12:16:01 -0500

    init recaptcha poc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
+import React from 'react';
+import { ReCaptcha, loadReCaptcha } from 'react-recaptcha-v3';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      recaptchaToken: ''
+    };
+  }
+  componentDidMount() {
+    loadReCaptcha(process.env.REACT_APP_RECAPTCHA_SITE_KEY);
+  }
+  updateRecaptchaToken() {
+    this.recaptcha.execute();
+  }
+  render() {
+    return (
+      <div className="App">
+        <ReCaptcha action='cr'
+          ref={ref => this.recaptcha = ref}
+          sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+          verifyCallback={(token) => {
+            console.log(token);
+            this.setState({
+              recaptchaToken: token
+            })
+          }}
+        />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={() => this.updateRecaptchaToken()}>Generar recaptcha</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
